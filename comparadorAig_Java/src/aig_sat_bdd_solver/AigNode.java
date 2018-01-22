f3e5f23cf3cb7a06575653450182e06fe6d425f0
Rodrigo Dal Ri <rodrigodalri1995@gmail.com>
2018-01-22 12:45:37 -0200

    comentários e identação

--- comparadorAig_Java/src/aig_sat_bdd_solver/AigNode.java
+++ comparadorAig_Java/src/aig_sat_bdd_solver/AigNode.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aig_sat_bdd_solver;
 
 /**
- *
- * @author luciano
+ * @author luciano/rodrigo
  */
+
 public class AigNode {
     int input1;
     int input2;
